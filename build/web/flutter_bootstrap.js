01c78c19f232e0ff559d4ba918e14dc7378cfaaa
sorndavin <davinsorn.dv@gmail.com>
2026-01-13 16:15:16 +0700

    yeee

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -34,6 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38deb","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38deb","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load();
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "758484072"
+  }
+});
